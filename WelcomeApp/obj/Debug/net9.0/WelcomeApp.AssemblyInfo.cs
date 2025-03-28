b2987eb2839b65e32e57be5cfee70a97d554d90c
Vaczlav <106527580+Vaczlav@users.noreply.github.com>
2025-03-28 10:58:28 -0400

    Add completed Anonymous Income Comparison Program

--- WelcomeApp/obj/Debug/net9.0/WelcomeApp.AssemblyInfo.cs
+++ WelcomeApp/obj/Debug/net9.0/WelcomeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WelcomeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b324440a228c1d32102cf992df4074e844ff435d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99da0d7c6745155761cadca4503b9cbb1569f764")]
 [assembly: System.Reflection.AssemblyProductAttribute("WelcomeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WelcomeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
